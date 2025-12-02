ddd56016b4924f91cd1e1dc256bb7c3b396724b9
Aibatyr <aibatyr111@email.com>
2025-12-02 18:08:58 +0500

    Initial commit: todo-app mini task 2

---
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -0,0 +1,27 @@
+package com.example.todoapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TaskController {
+
+    @GetMapping("/tasks")
+    public List<Map<String, Object>> getTasks() {
+        // Простой список задач "в памяти", без БД
+        return Arrays.asList(
+                Map.of("id", 1, "title", "Buy milk", "done", false),
+                Map.of("id", 2, "title", "Do homework", "done", true),
+                Map.of("id", 3, "title", "Walk the dog", "done", false)
+        );
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "Todo app is running. Check /tasks";
+    }
+}
